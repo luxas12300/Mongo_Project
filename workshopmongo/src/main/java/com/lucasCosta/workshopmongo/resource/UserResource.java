8365e46cd18d00d6d65025ae2ebc67802deb01dd
Lucas Costa <costalucas017@gmail.com>
2026-03-02 21:38:48 -0300

    Endpoit to return user's posts

--- workshopmongo/src/main/java/com/lucasCosta/workshopmongo/resource/UserResource.java
+++ workshopmongo/src/main/java/com/lucasCosta/workshopmongo/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.lucasCosta.workshopmongo.resource;
 
+import com.lucasCosta.workshopmongo.domain.Post;
 import com.lucasCosta.workshopmongo.domain.User;
 import com.lucasCosta.workshopmongo.dto.UserDTO;
 import com.lucasCosta.workshopmongo.service.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>>findPosts(@PathVariable String id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
     }
 }
